a56a7ae1f756127a9b8f29bac78b8f752bc49452
f8B23Lef <mal7rish@yandex.ru>
2018-09-23 17:57:37 +0300

    smart-calculator/incomplete solution

--- src/index.js
+++ src/index.js
@@ -1,27 +1,125 @@
 class SmartCalculator {
   constructor(initialValue) {
-    // your implementation
+    this.priority = {
+      '+': 1,
+      '-': 1,
+      '*': 2,
+      '/': 2,
+      '**': 3
+    };
+    
+    this.operator = [];
+    this.operand = [];
+    
+    this.operand.push(initialValue);
+
+    this.result = -1;
   }
 
   add(number) {
-    // your implementation
+    var plus = '+';
+    if(!this.operator.length) {
+      this.operator.push(plus);	
+    } else {
+      this.checkPriority(plus);
+    }
+    this.operand.push(number);
+    return this;
   }
   
   subtract(number) {
-    // your implementation
+    var minus = '-';
+    if(!this.operator.length) {
+      this.operator.push(minus);	
+    } else {
+      this.checkPriority(minus);
+    }
+    this.operand.push(number);
+    return this;
   }
 
   multiply(number) {
-    // your implementation
+    var mult = '*';
+    if(!this.operator.length) {
+      this.operator.push(mult);	
+    } else {
+      this.checkPriority(mult);
+    }
+    this.operand.push(number);
+    return this;
   }
 
   devide(number) {
-    // your implementation
+    var div = '/';
+    if(!this.operator.length) {
+      this.operator.push(div);	
+    } else {
+      this.checkPriority(div);
+    }
+    this.operand.push(number);
+    return this;
   }
 
   pow(number) {
-    // your implementation
+    var pow = '**';
+    if(!this.operator.length) {
+      this.operator.push(pow);	
+    } else {
+      this.checkPriority(pow);
+    }
+    this.operand.push(number);
+    return this;
   }
+
+  checkPriority(curOperator) {
+    var lastOperator = this.operator[this.operator.length - 1];
+    var lastOperatorPriority = this.priority[lastOperator];
+
+    var curOperatorPriority = this.getPriority(curOperator);
+
+    while(this.operator.length && lastOperatorPriority >= curOperatorPriority) {  
+      this.operand.push(this.operator.pop());
+      lastOperator = this.operator[this.operator.length - 1];
+      lastOperatorPriority = this.getPriority(lastOperator);  
+    }
+    this.operator.push(curOperator);
+  }
+
+  getPriority(operator) {
+    return this.priority[operator];
+  }
+
+  toString() {//toString()
+    while(this.operator.length) {
+      this.operand.push(this.operator.pop());
+    }
+    this.result = calculate(this.operand);
+
+    return this.result;
+  }
+}
+
+function calculate(rpn) { 
+
+  const operators = {
+    '+': (x, y) => x + y,
+    '-': (x, y) => x - y,
+    '*': (x, y) => x * y,
+    '/': (x, y) => x / y,
+    '**': (x, y) => x ** y
+  };
+
+  var stack = [];
+  
+  for(var i = 0; i < rpn.length; i++) {
+    if(rpn[i] in operators) {
+      var [y, x] = [stack.pop(), stack.pop()];
+      stack.push(operators[rpn[i]](x, y));
+    } else {
+      stack.push(rpn[i]);
+    }
+  }
+  return stack.pop(); 
 }
 
 module.exports = SmartCalculator;
